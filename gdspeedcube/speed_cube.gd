0070dffe5edb569adbf134f787f19a9a88bbae4f
Cass Everitt <cass@xyzw.us>
2025-09-01 20:57:47 -0600

    instantiate all the pieces

--- gdspeedcube/speed_cube.gd
+++ gdspeedcube/speed_cube.gd
@@ -1,7 +1,16 @@
 @tool
 extends Node3D
 
-@onready var piece = $Piece0
+@onready var piece = $Piece
+
+var rc : Array
 
 func _ready() -> void:
-	pass
+	for i in 3:
+		for j in 3:
+			for k in 3:
+				var p : CSGBox3D = piece.duplicate()
+				p.position += p.size * (Vector3(i, j, k) - Vector3(1, 1, 1))
+				rc.append(p)
+				self.add_child(p)
+	self.remove_child(piece)
