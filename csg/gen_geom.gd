6c8bf7c8d991610e4712452736a3ba8fac61b59a
Cass Everitt <cass@xyzw.us>
2025-09-03 23:03:12 -0600

    Got copilot to produce the correct code this time, I think

--- csg/gen_geom.gd
+++ csg/gen_geom.gd
@@ -11,18 +11,16 @@ class Edge:
 	var v1: Vertex
 	var v2: Vertex
 	var faces = []
-	var sharpness: float = 0.0 # 0 = smooth, 1 = sharp, between is semi-sharp
+	var sharpness: float = 0.0
 
 class Face:
 	var verts = []
 	var edges = []
 	var id: int
 
-# Utility: Lerp between two vectors
 func lerp(a: Vector3, b: Vector3, t: float) -> Vector3:
 	return a * (1.0 - t) + b * t
 
-# Build cube topology
 func generate_cube_topology() -> Dictionary:
 	var verts = []
 	var edges = []
@@ -31,26 +29,31 @@ func generate_cube_topology() -> Dictionary:
 	var edge_map = {}
 
 	var cube_vertices = [
-		Vector3(-1, -1, -1),
-		Vector3( 1, -1, -1),
-		Vector3( 1,  1, -1),
-		Vector3(-1,  1, -1),
-		Vector3(-1, -1,  1),
-		Vector3( 1, -1,  1),
-		Vector3( 1,  1,  1),
-		Vector3(-1,  1,  1),
+		Vector3(-1, -1, -1), # 0
+		Vector3( 1, -1, -1), # 1
+		Vector3( 1,  1, -1), # 2
+		Vector3(-1,  1, -1), # 3
+		Vector3(-1, -1,  1), # 4
+		Vector3( 1, -1,  1), # 5
+		Vector3( 1,  1,  1), # 6
+		Vector3(-1,  1,  1), # 7
 	]
-	# Faces as lists of 4 vertex indices (quads)
+	# Each face: indices are in CCW order when viewed from outside
+	# -Z: front
+	# +Z: back
+	# -Y: bottom
+	# +Y: top
+	# -X: left
+	# +X: right
 	var cube_faces = [
-		[0, 1, 2, 3], # Bottom (-Z)
-		[4, 5, 6, 7], # Top (+Z)
-		[0, 1, 5, 4], # Front (-Y)
-		[2, 3, 7, 6], # Back (+Y)
-		[0, 3, 7, 4], # Left (-X)
-		[1, 2, 6, 5], # Right (+X)
+		[0, 1, 2, 3], # -Z (front) -- OK
+		[5, 4, 7, 6], # +Z (back)   -- FIXED (was [4,5,6,7])
+		[0, 4, 5, 1], # -Y (bottom) -- FIXED (was [0,1,5,4])
+		[3, 2, 6, 7], # +Y (top)    -- FIXED (was [2,3,7,6])
+		[0, 3, 7, 4], # -X (left)   -- OK
+		[1, 5, 6, 2], # +X (right)  -- OK
 	]
 
-	# Create vertex objects
 	for i in cube_vertices.size():
 		var v = Vertex.new()
 		v.pos = cube_vertices[i]
@@ -58,7 +61,6 @@ func generate_cube_topology() -> Dictionary:
 		verts.append(v)
 		vert_map[i] = v
 
-	# Create faces and edges, build connectivity
 	for fi in cube_faces.size():
 		var face_indices = cube_faces[fi]
 		var f = Face.new()
@@ -67,7 +69,6 @@ func generate_cube_topology() -> Dictionary:
 			var vi = face_indices[i]
 			f.verts.append(vert_map[vi])
 			vert_map[vi].faces.append(f)
-		# Add edges per face, avoid duplicates
 		for i in range(4):
 			var vi1 = face_indices[i]
 			var vi2 = face_indices[(i+1)%4]
@@ -88,8 +89,6 @@ func generate_cube_topology() -> Dictionary:
 			f.edges.append(edge)
 		faces.append(f)
 
-	# Example: Mark all cube edges semi-sharp (0.5)
-	# You can customize sharpness per edge here
 	for edge in edges:
 		edge.sharpness = 0.5
 
@@ -99,7 +98,12 @@ func generate_cube_topology() -> Dictionary:
 		"faces": faces
 	}
 
-# One iteration of Catmull-Clark subdivision (returns new mesh topology)
+func catmull_clark_subdivide_multi(mesh_topo: Dictionary, iterations: int) -> Dictionary:
+	var topo = mesh_topo
+	for i in iterations:
+		topo = catmull_clark_subdivide(topo)
+	return topo
+
 func catmull_clark_subdivide(mesh_topo: Dictionary) -> Dictionary:
 	var old_verts = mesh_topo["vertices"]
 	var old_edges = mesh_topo["edges"]
@@ -109,7 +113,6 @@ func catmull_clark_subdivide(mesh_topo: Dictionary) -> Dictionary:
 	var edge_points = {}
 	var vertex_points = []
 
-	# 1. Compute face points
 	for f in old_faces:
 		var avg = Vector3.ZERO
 		for v in f.verts:
@@ -117,35 +120,28 @@ func catmull_clark_subdivide(mesh_topo: Dictionary) -> Dictionary:
 		avg /= f.verts.size()
 		face_points.append(avg)
 
-	# 2. Compute edge points
 	for e in old_edges:
 		var fp_sum = Vector3.ZERO
 		for f in e.faces:
 			fp_sum += face_points[old_faces.find(f)]
 		fp_sum /= e.faces.size()
 		var ep = (e.v1.pos + e.v2.pos + fp_sum) / (2 + e.faces.size())
-		# Semi-sharp: interpolate between Catmull-Clark and sharp (just edge midpoint)
 		if e.sharpness > 0.0:
 			var sharp_ep = (e.v1.pos + e.v2.pos) * 0.5
 			ep = lerp(ep, sharp_ep, e.sharpness)
 		edge_points[e] = ep
 
-	# 3. Compute new vertex points
 	for v in old_verts:
-		# Face points
 		var F = Vector3.ZERO
 		for f in v.faces:
 			F += face_points[old_faces.find(f)]
 		F /= v.faces.size()
-		# Edge midpoints
 		var R = Vector3.ZERO
 		for e in v.edges:
 			R += edge_points[e]
 		R /= v.edges.size()
-		# Original position
 		var n = v.faces.size()
 		var new_v = (F + 2*R + (n-3)*v.pos) / n
-		# Semi-sharp: if any adjacent edge is sharp, interpolate to original pos
 		var max_sharp = 0.0
 		for e in v.edges:
 			max_sharp = max(max_sharp, e.sharpness)
@@ -153,15 +149,8 @@ func catmull_clark_subdivide(mesh_topo: Dictionary) -> Dictionary:
 			new_v = lerp(new_v, v.pos, max_sharp)
 		vertex_points.append(new_v)
 
-	# Build new topology
-	# For each old face, create 4 new faces (quads)
-	# Each quad is made from: old vertex, two edge points, face point
-
 	var new_vertices = []
-	var new_faces = []
-	var vertex_map = {} # Map for deduplication
-
-	# Helper to get or create vertex
+	var vertex_map = {}
 	var get_vertex = func(pos: Vector3) -> int:
 		var key = str(pos)
 		if vertex_map.has(key):
@@ -171,7 +160,6 @@ func catmull_clark_subdivide(mesh_topo: Dictionary) -> Dictionary:
 		vertex_map[key] = idx
 		return idx
 
-	# Store new quads as indices into new_vertices
 	var new_quads = []
 
 	for fi in old_faces.size():
@@ -179,68 +167,98 @@ func catmull_clark_subdivide(mesh_topo: Dictionary) -> Dictionary:
 		var fp = face_points[fi]
 		for i in range(f.verts.size()):
 			var v0 = f.verts[i]
-			var v1 = f.verts[(i+1)%f.verts.size()]
+			var _v1 = f.verts[(i+1)%f.verts.size()]
 			var e0 = f.edges[i]
 			var e1 = f.edges[(i-1+f.edges.size())%f.edges.size()]
-			# The quad:
-			# - v0 subdivided vertex
-			# - edge point between v0 and v1
-			# - face point
-			# - edge point between v0 and previous vertex
 			var idx_v0 = get_vertex.call(vertex_points[old_verts.find(v0)])
 			var idx_ep0 = get_vertex.call(edge_points[e0])
 			var idx_fp = get_vertex.call(fp)
 			var idx_ep1 = get_vertex.call(edge_points[e1])
 			new_quads.append([idx_v0, idx_ep0, idx_fp, idx_ep1])
 
+	var verts_objs = []
+	for i in new_vertices.size():
+		var vert = Vertex.new()
+		vert.pos = new_vertices[i]
+		vert.id = i
+		verts_objs.append(vert)
+
+	var faces_objs = []
+	var edges_map = {}
+	var edges_objs = []
+	for fi in new_quads.size():
+		var quad = new_quads[fi]
+		var f = Face.new()
+		f.id = fi
+		for i in range(4):
+			var vi = quad[i]
+			f.verts.append(verts_objs[vi])
+			verts_objs[vi].faces.append(f)
+		for i in range(4):
+			var vi1 = quad[i]
+			var vi2 = quad[(i+1)%4]
+			var edge_key = [min(vi1,vi2), max(vi1,vi2)]
+			var edge_key_str = str(edge_key)
+			var edge
+			if edges_map.has(edge_key_str):
+				edge = edges_map[edge_key_str]
+			else:
+				edge = Edge.new()
+				edge.v1 = verts_objs[edge_key[0]]
+				edge.v2 = verts_objs[edge_key[1]]
+				edges_map[edge_key_str] = edge
+				edges_objs.append(edge)
+				edge.v1.edges.append(edge)
+				edge.v2.edges.append(edge)
+			edge.faces.append(f)
+			f.edges.append(edge)
+		faces_objs.append(f)
+
+	for edge in edges_objs:
+		edge.sharpness = 0.5
+
 	return {
-		"vertices": new_vertices,
-		"quads": new_quads
+		"vertices": verts_objs,
+		"edges": edges_objs,
+		"faces": faces_objs
 	}
 
-# Build ArrayMesh from quad mesh (with smooth normals)
 func build_mesh_from_quads(vertices: Array, quads: Array) -> ArrayMesh:
 	var st = SurfaceTool.new()
 	st.begin(Mesh.PRIMITIVE_TRIANGLES)
-	# Compute normals per vertex, for smooth shading
 	var normals = []
 	for i in range(vertices.size()):
 		normals.append(Vector3.ZERO)
 	for quad in quads:
 		var v_indices = quad
-		var v0 = vertices[v_indices[0]]
-		var v1 = vertices[v_indices[1]]
-		var v2 = vertices[v_indices[2]]
-		var v3 = vertices[v_indices[3]]
-		# Triangles: (v0, v1, v2), (v0, v2, v3)
+		var v0 = vertices[v_indices[0]].pos
+		var v1 = vertices[v_indices[1]].pos
+		var v2 = vertices[v_indices[2]].pos
+		var v3 = vertices[v_indices[3]].pos
 		var n1 = Plane(v0, v1, v2).normal
 		var n2 = Plane(v0, v2, v3).normal
 		normals[v_indices[0]] += n1 + n2
 		normals[v_indices[1]] += n1
 		normals[v_indices[2]] += n1 + n2
 		normals[v_indices[3]] += n2
-	# Normalize normals
 	for i in range(normals.size()):
 		normals[i] = normals[i].normalized()
-	# Add triangles
 	for quad in quads:
 		var v_indices = quad
-		var v0 = vertices[v_indices[0]]
-		var v1 = vertices[v_indices[1]]
-		var v2 = vertices[v_indices[2]]
-		var v3 = vertices[v_indices[3]]
+		var v0 = vertices[v_indices[0]].pos
+		var v1 = vertices[v_indices[1]].pos
+		var v2 = vertices[v_indices[2]].pos
+		var v3 = vertices[v_indices[3]].pos
 		var n0 = normals[v_indices[0]]
 		var n1 = normals[v_indices[1]]
 		var n2 = normals[v_indices[2]]
 		var n3 = normals[v_indices[3]]
-		# First triangle
 		st.set_normal(n0)
 		st.add_vertex(v0)
 		st.set_normal(n1)
 		st.add_vertex(v1)
 		st.set_normal(n2)
 		st.add_vertex(v2)
-		# Second triangle
 		st.set_normal(n0)
 		st.add_vertex(v0)
 		st.set_normal(n2)
@@ -249,8 +267,16 @@ func build_mesh_from_quads(vertices: Array, quads: Array) -> ArrayMesh:
 		st.add_vertex(v3)
 	return st.commit()
 
-func _run() -> void:
+func _run():
+	var iterations = 2 # Change for more/less subdivision levels
 	var topo = generate_cube_topology()
-	var subdiv = catmull_clark_subdivide(topo)
-	var mesh = build_mesh_from_quads(subdiv["vertices"], subdiv["quads"])
+	var subdiv_topo = catmull_clark_subdivide_multi(topo, iterations)
+	var quads = []
+	for f in subdiv_topo["faces"]:
+		var quad = []
+		for v in f.verts:
+			quad.append(v.id)
+		quads.append(quad)
+	var mesh = build_mesh_from_quads(subdiv_topo["vertices"], quads)
 	ResourceSaver.save(mesh, "res://mesh.tres")
+	print("Mesh saved to res://mesh.tres")
