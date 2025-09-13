087311a881fb3fab2af08de5604256e51a0a5627
Cass Everitt <cass@xyzw.us>
2025-09-13 01:20:14 -0600

    simplify the interior piece face calculation

--- gdspeedcube/speed_cube.gd
+++ gdspeedcube/speed_cube.gd
@@ -6,16 +6,19 @@ extends Node3D
 var rc : Array
 
 func _ready() -> void:
-	self.scale *= 0.1
+	self.scale *= 1.0
+	var piece_scale = 0.333;
+	mat.set_shader_parameter("piece_scale", piece_scale)
 	for i in 3:
 		for j in 3:
 			for k in 3:
 				var mi := MeshInstance3D.new()
-				mi.scale *= 0.75
+				mi.scale *= piece_scale * 0.8
 				mi.mesh = piece.duplicate()
 				mi.material_override = mat
-				mi.position += (Vector3(i, j, k) - Vector3(1, 1, 1))
+				mi.position += (Vector3(i, j, k) - Vector3(1, 1, 1)) * piece_scale
 				var idx = i + j * 4 + k * 16
 				mi.set_instance_shader_parameter("piece_idx", idx)
+				mi.set_instance_shader_parameter("piece_bias", mi.position)
 				rc.append(mi)
 				self.add_child(mi)
